f1420e807328bfc7ba1c07671a6004bafab6da9d
forhadisrafil <forhadtuhin5088@gmail.com>
2020-10-02 23:39:45 +0600

    update practice

--- beautifulsoup_practice.py
+++ beautifulsoup_practice.py
@@ -47,7 +47,7 @@ for image in images:
 bs.find_all(lambda tag: len(tag.attrs) == 2)
 bs.find_all(lambda tag: tag.get_text() == 'Or maybe he\'s only resting?')
 bs.find_all('', text='Or maybe he\'s only resting?')
-
+bs.find_all('div', attrs={'class': 'card-content'})
 for link in bs.find_all('a', limit=20):
     if 'href' in link.attrs:
         print(link.attrs['href'])
